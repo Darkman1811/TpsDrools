9083495438b821d8d20e5045226c2f0224290d4a
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-06-25 23:05:00 +0000

    Tp1 using working memory

---
+++ TestDrools/src/main/java/sn/guestu/testdrools/Product.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sn.guestu.testdrools;
+
+/**
+ *
+ * @author super
+ */
+public class Product {
+    private String type;
+	private int discount;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+}
